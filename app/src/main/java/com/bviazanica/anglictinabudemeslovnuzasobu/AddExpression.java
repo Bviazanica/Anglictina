15f72b9bd0a9b459e3e8719ec10f55e2aaab9976
Branislav Viazanica <dev@praxuj.sk>
2021-01-29 21:31:46 +0100

    dialog for testing language, action bar, intents for mainactivity

--- app/src/main/java/com/bviazanica/anglictinabudemeslovnuzasobu/AddExpression.java
+++ app/src/main/java/com/bviazanica/anglictinabudemeslovnuzasobu/AddExpression.java
@@ -2,17 +2,19 @@ package com.bviazanica.anglictinabudemeslovnuzasobu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashMap;
 
 public class AddExpression extends AppCompatActivity {
 
+    //        dropdown hint/ placeholder bude list[0] disabled, plusko nejak zmenit(material design maybe)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expression);
-
-
-
-//        dropdown hint/ placeholder bude list[0] disabled, plusko nejak zmenit(material design maybe)
     }
 }
--- app/src/main/java/com/bviazanica/anglictinabudemeslovnuzasobu/MainActivity.java
+++ app/src/main/java/com/bviazanica/anglictinabudemeslovnuzasobu/MainActivity.java
@@ -1,20 +1,86 @@
 package com.bviazanica.anglictinabudemeslovnuzasobu;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Intent intent = new Intent();
+    String selectedTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //pridaj slovo => Add expression => modal na kategoriu
-
-        //MOJ SLOVNIK => Vocabulary, po klik na kategoriu => CategoryContent
-
-        //START TESTING bude mat dialog na výber ENG -> SVK ; SVK -> ENG a potom => StartTestingActivity
+        Button[] btn = new Button[3];
+        btn[0] = findViewById(R.id.add_expression_btn);
+        btn[1] = findViewById(R.id.vocabulary_btn);
+        btn[2] = findViewById(R.id.start_test_btn);
+        for (int i = 0; i < 3; i++) {
+            btn[i].setOnClickListener(this);
+        }
     }
+    @Override
+    public void onClick(View v) {
+        if (v == findViewById(R.id.add_expression_btn)) {
+            intent = new Intent(MainActivity.this,
+                    AddExpression.class);
+        } else if (v == findViewById(R.id.vocabulary_btn)) {
+            intent = new Intent(MainActivity.this,
+                    Vocabulary.class);
+        } else if (v == findViewById(R.id.start_test_btn)) {
+            showAlertDialog();
+        }
+
+    }
+
+    //pridaj slovo => Add expression => modal na kategoriu
+
+    //MOJ SLOVNIK => Vocabulary, po klik na kategoriu => CategoryContent
+
+    //START TESTING bude mat dialog na výber ENG -> SVK ; SVK -> ENG a potom => StartTestingActivity
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.import_data:
+            case R.id.export_data:
+                Toast.makeText(this, item.toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void showAlertDialog() {
+        String[] testLanguages = {getString(R.string.english), getString(R.string.slovak)};
+        selectedTest = getString(R.string.english);
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialogTitle))
+                .setSingleChoiceItems(testLanguages, 0, (dialog, which) -> {
+                    selectedTest = testLanguages[which];
+                    System.out.println("selected je " + selectedTest);
+                })
+                .setPositiveButton(getString(R.string.confirm), (dialog, which) -> {
+                    Intent intent = new Intent(MainActivity.this, StartTesting.class);
+                    startActivity(intent);
+                    System.out.println(selectedTest);
+                })
+
+                .setNegativeButton(getString(R.string.cancel), null)
+                .show();
+    }
+
 }
